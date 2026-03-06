0985652afcf3ac21ded7460d05923749d14f996f
SLG <slgwfhpersonal@email.com>
2026-03-06 17:04:45 +0530

    Initial commit - basic app

---
+++ README.md
@@ -0,0 +1,3 @@
+# GH-PipelineProApp
+
+A very basic app to learn GitHub CI/CD with Playwright and Azure DevOps bug creation.
---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("statusBtn").addEventListener("click", () => {
+  document.getElementById("result").textContent = "App is working fine.";
+});
